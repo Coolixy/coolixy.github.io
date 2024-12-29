765b8d34f9c1449f8e5eadcebfe41e7ceaf901ca
Coolixy <saatvikisro@gmail.com>
2024-12-29 14:31:39 +0530

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+const myImage=document.querySelector("img");
+
+myImage.addEventListener("click",() => {
+
+    const mySrc = myImage.getAttribute("src");
+    if(mySrc==="images/fox.jpg") myImage.setAttribute("src","images/fox2.jpg");
+    else myImage.setAttribute("src","images/fox.jpg");
+});
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName(){
+    const myName=prompt("Please enter your name.");
+    localStorage.setItem("name",myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
+
+if(!localStorage.getItem("name")) setUserName();
+else{
+    const storedName= localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+myButton.addEventListener('click',()=>{
+    setUserName();
+});
